aacd8cdd01e7530d48095f186aa5676edb8d582d
Eiln <ebostrom@live.se>
2020-09-22 16:28:55 +0200

    Create fetch for register user

--- src/data/UserKit.js
+++ src/data/UserKit.js
@@ -3,4 +3,28 @@ const ROOT_URL = "https://frebi.willandskill.eu/"
 
 export default class {
 
+  async register(firstName, lastName, email, password, organisationName, organisationKind) {
+    const url = `${ROOT_URL}auth/users/`
+    const payload = {
+      firstName,
+      lastName,
+      email,
+      password,
+      organisationName,
+      organisationKind
+    }
+
+    return fetch(url, {
+      method: "POST",
+      headers: this.getPublicHeaders(),
+      body: JSON.stringify(payload)
+    })
+  }
+
+  getPublicHeaders() {
+    return {
+      "Content-Type": "application/json"
+    }
+  }
+
 }
