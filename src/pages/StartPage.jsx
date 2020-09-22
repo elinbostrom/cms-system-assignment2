ea395b91e5416738e16bfb885b09eb02e48f534a
Eiln <ebostrom@live.se>
2020-09-22 16:30:11 +0200

    Add register button

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default function StartPage() {
+  const history = useHistory()
+
   return (
     <div>
       <h1>Startpage</h1>
+      <button onClick={() => { history.push("/register") }}>Sign up</button>
     </div>
   )
 }
