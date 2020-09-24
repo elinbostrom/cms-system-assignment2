1a72dfcf12b82a4a766461d65325587ea202f188
Eiln <ebostrom@live.se>
2020-09-24 21:47:59 +0200

    Renamed AdminPage to HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import UserKit from '../data/UserKit'
+
+export default function HomePage() {
+  const userKit = new UserKit()
+  const [customerList, setCustomerList] = useState([])
+  const history = useHistory()
+
+
+  const fetchCustomerList = () => {
+    userKit.fetchCustomers()
+      .then(res => res.json())
+      .then(data => {
+        setCustomerList(data.results)
+      })
+  }
+
+
+  useEffect(() => {
+    fetchCustomerList() // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div>
+      <h1>Home page</h1>
+      <button onClick={() => { history.push("/create-customer") }}>Create new customer</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer name</th>
+            <th>Organisation Number</th>
+            <th>Reference</th>
+            <th>Read more</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customerList && customerList.map(customerItem => {
+            return (
+              <tr key={customerItem.id}>
+                <td>{customerItem.name}</td>
+                <td>{customerItem.organisationNr}</td>
+                <td>{customerItem.reference}</td>
+                <td><Link to={`/customers/${customerItem.id}`}>Click</Link></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
