ceac2941ead4336ffb3df0c2ddfd58a8a04ebebe
Eiln <ebostrom@live.se>
2020-09-26 20:22:28 +0200

    Fetch information directly when page renders

--- src/components/ListCustomers.jsx
+++ src/components/ListCustomers.jsx
@@ -16,6 +16,7 @@ export default function ListCustomers() {
   }
 
   useEffect(() => {
+    userKit.fetchCustomers()
     fetchCustomerList() // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
