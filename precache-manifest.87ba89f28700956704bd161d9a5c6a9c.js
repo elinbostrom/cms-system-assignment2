f136df123d596708f88636e3b7b24db84dc52821
Eiln <ebostrom@live.se>
2020-09-29 14:33:36 +0200

    Updates

---
+++ precache-manifest.87ba89f28700956704bd161d9a5c6a9c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdad0d7555e1a5fb6e04eaf4db077d9",
+    "url": "/crm-system-assignment2/index.html"
+  },
+  {
+    "revision": "4ad840a7d6f8b666a57c",
+    "url": "/crm-system-assignment2/static/js/2.51979601.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/crm-system-assignment2/static/js/2.51979601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951b0034e1f3f9cb1a2f",
+    "url": "/crm-system-assignment2/static/js/main.94b3e197.chunk.js"
+  },
+  {
+    "revision": "a79b323d80eff0f2b36d",
+    "url": "/crm-system-assignment2/static/js/runtime-main.8bea7cf1.js"
+  },
+  {
+    "revision": "1f7b1be1ca832496d3092700527070bc",
+    "url": "/crm-system-assignment2/static/media/background-startpage.1f7b1be1.jpg"
+  },
+  {
+    "revision": "562250f9e4b2c9c1c744705e37f641ba",
+    "url": "/crm-system-assignment2/static/media/customer-list.562250f9.png"
+  },
+  {
+    "revision": "797488f9380c39c40b4120083ab02ad0",
+    "url": "/crm-system-assignment2/static/media/edit-customer.797488f9.png"
+  },
+  {
+    "revision": "f67ae184ecebc946bbc42e91ee78fbba",
+    "url": "/crm-system-assignment2/static/media/logo-black.f67ae184.png"
+  },
+  {
+    "revision": "6b2bc12558691a33f6306d2adfd601d6",
+    "url": "/crm-system-assignment2/static/media/logo-white.6b2bc125.png"
+  }
+]);
